4155515c78b1c159ae8a1c4f0c2da68c70f10179
MichelAngelo <baksdevelopmen@gmail.com>
2025-05-06 23:12:56 +0300

    BaksDevelopment

--- Messenger/TelegramManufacturePart/TelegramManufacturePartDone.php
+++ Messenger/TelegramManufacturePart/TelegramManufacturePartDone.php
@@ -172,11 +172,11 @@ final readonly class TelegramManufacturePartDone
         $messageHandler = '<b>Выполненный этап производственной партии:</b>';
         $messageHandler .= PHP_EOL;
         $messageHandler .= PHP_EOL;
-        $messageHandler .= sprintf('Номер: <b>%s</b>', $ManufacturePartEvent->getManufacturePartNumber()); // номер партии
+        $messageHandler .= sprintf('Номер: <b>%s</b>', $ManufacturePartEvent->getInvariable()->getNumber()); // номер партии
         $messageHandler .= PHP_EOL;
         $messageHandler .= sprintf('Дата: <b>%s</b>', new DateTimeImmutable()->format('d.m.Y H:i')); // Дата выполненного этапа
         $messageHandler .= PHP_EOL;
-        $messageHandler .= sprintf("%s: <b>%s шт.</b>", $UsersTableActionsWorkingUid->getAttr(), $ManufacturePartEvent->getManufacturePartQuantity()); // Этап производства
+        $messageHandler .= sprintf("%s: <b>%s шт.</b>", $UsersTableActionsWorkingUid->getAttr(), $ManufacturePartEvent->getInvariable()->getQuantity()); // Этап производства
 
         /** Отправляем сообщение об успешном выполнении этапа */
         $this
